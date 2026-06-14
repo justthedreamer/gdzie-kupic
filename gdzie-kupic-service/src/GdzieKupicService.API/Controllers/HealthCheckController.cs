aedfd41b8ad6e75b9868f969d174120796ad9c89
Piotr Zapala <piotr.zapala.dev@gmail.com>
2026-06-14 15:39:08 +0200

    setup dockerfile and add healtcheck endpoint
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ gdzie-kupic-service/src/GdzieKupicService.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,10 @@
+﻿namespace GdzieKupicService.API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => this.Ok("Gdzie Kupic Service is running.");
+}
